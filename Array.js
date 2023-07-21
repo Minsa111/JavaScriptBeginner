43254bef85b981ffc5724d5946b2b0b95c3524ef
Minsa111 <dragonriderrgt@gmail.com>
2023-07-22 00:23:00 +0700

    first commit

---
+++ Array.js
@@ -0,0 +1,4 @@
+const evenNumber=[];
+for(let i=2; i<=100; i += 2){
+    evenNumber.push(i);
+}
---
+++ Object.js
@@ -0,0 +1,10 @@
+const restaurant = {
+    name : "Bakso Mang Dicoding",
+    city : "Bandung",
+    "favorite drink": "Es Teh",
+    isVegan: false
+};
+let name;
+name = restaurant.name;
+let favoriteDrink;
+favoriteDrink= restaurant["favorite drink"];
